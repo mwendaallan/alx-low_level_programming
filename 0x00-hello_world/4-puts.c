520be7ffc97ef57940a00313238e1f66ce5d5fb2
mwendaallan <myemail@gmail.com>
2023-02-16 12:01:20 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a file with puts function
+ * Return: 0 (Success)
  */
 int main(void)
-(
-puts("\"Programming is like building a multilingual puzzle,");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-)
+}
